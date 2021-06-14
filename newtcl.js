ea417fb2916056f8c36b0f886e508fcb587e0172
PerthGoat <administrator@ryancdeyong.us>
2021-06-13 21:27:21 -0400

    more error catching

--- newtcl.js
+++ newtcl.js
@@ -1,176 +1,12 @@
 let tcsr = `
-proc for {initCmd testExpr advanceCmd bodyScript} {
-  uplevel $initCmd
-  set testCmd [list expr $testExpr]
-  while {[uplevel $testCmd]} {
-    uplevel $bodyScript
-    uplevel $advanceCmd
-  }
-}
-
-proc incr {x} {
-  set listexpr [uplevel [list expr "\\$$x" 1 +]]
-  uplevel [list set $x $listexpr]
-}
-
-# compute POWER of x to the y
-proc pow {x y} {
-  if {$y 0 =} {
-    return 1
-  }
-  
-  set res $x
-  for {set i 1} {$i $y <} {incr i} {
-    
-    set res [expr $res $x *]
-  }
-  return $res
-}
-
-proc factorial {x} {
-  set fact 1
-  for {} {$x 1 >} {set x [expr $x 1 -]} {
-    set fact [expr $fact $x *]
-  }
-
-  return $fact
-}
-
-# compute sqrt of n
-# this is a poor algorithm for it, it just gets it approximately correct in 10 iterations
-proc sqrt {n} {
-  # run for 10 iterations
-  
-  set est 0
-  
-  set cness 0
-  
-  for {set i 0} {$i 10 <} {incr i} {
-    set cness [pow $est 2]
-    # negative diff is too high
-    # positive diff is too low
-    set diff [expr $n $cness -]
-    set smalldiff [expr $diff 10 /]
-    #puts $smalldiff
-    
-    set est [expr $est $smalldiff +]
-    
-    #puts $est
-  }
-  
-  return $est
-}
-
-# arctangent of x using the taylor series
-proc arctan {x} {
-  
-  set xsum 0
-  set flip 0
-  
-  for {set i 0} {$i 50 <} {incr i} { 
-    set pw [expr $i 2 *]
-    set pw [expr $pw 1 +]
-    
-    set xt [pow $x $pw]
-    
-    set xd [expr $xt $pw /]
-    
-    if {$flip 0 =} {
-      set xsum [expr $xsum $xd +]
-      set flip 1
-    } {
-      set xsum [expr $xsum $xd -]
-      set flip 0
-    }
-    
-    #puts $xd
-  }
-  return $xsum
-}
-
-
-# get PI
-proc PI {} {
-  set fr1 [expr 1 5 /]
-  set fr1 [arctan $fr1]
-  set fr1 [expr $fr1 4 *]
-  set fr2 [expr 1 239 /]
-  set fr2 [arctan $fr2]
-  
-  set pi4 [expr $fr1 $fr2 -]
-  
-  set pi [expr $pi4 4 *]
-  
-  return $pi
-}
-
-# override PI with a default val from wolfram
-proc PI {} {
-  return 3.14159265358979323846264338327950288
-}
-
-proc sin {x} {
-  set xsum 0
-  set flip 0
-  
-  for {set i 0} {$i 10 <} {incr i} {
-    set pw [expr $i 2 *]
-    set pw [expr $pw 1 +]
-    set fact [factorial $pw]
-    
-    set xt [pow $x $pw]
-    
-    set xd [expr $xt $fact /]
-    
-    if {$flip 0 =} {
-      set xsum [expr $xsum $xd +]
-      set flip 1
-    } {
-      set xsum [expr $xsum $xd -]
-      set flip 0
-    }
-    
-    #puts $xd
-  }
-  
-  return $xsum
-}
-
-proc cos {x} {
-  set halfpi [PI]
-  set halfpi [expr $halfpi 2 /]
-  set sum [expr $halfpi $x +]
-  return [sin $sum]
-}
-
-proc tan {x} {
-  set s [sin $x]
-  set c [cos $x]
-  
-  return [expr $s $c /]
-}
-
-proc arrayTest {name} {
-  aba
-  for {set i 0} {$i 3 <} {incr i} {
-    puts [tan $name($i)]
-  }
-}
-
-set x(0) 0
-set x(1) 1
-set x(2) 2
-
-
-
-# puts $x(1)
-
-#arrayTest $x
-# puts [tan 4]`;
+puts [appa]
+`;
 
 function isAlphaNum(s) {
   return s.match(/^[a-z0-9+-/*.=><()$#]+$/i);
 }
+
+let ctx = document.getElementById('screenout').getContext('2d');
 
 let varstack = [];
 let procstack = {};
@@ -316,6 +152,18 @@ function runCmd(cl) {
     case 'puts':
     logColor('\t\n\t' + cl[1], '#BAF');
     addToResultBox('\=> ' + cl[1]);
+    break;
+    case 'putpixel': // putpixel x y r g b
+    //logColor(`\t\n\t ${cl[1]} ${cl[2]} ${cl[3]} ${cl[4]} ${cl[5]}`, '#F00');
+    let coordx = cl[1];
+    let coordy = cl[2];
+    let r = cl[3];
+    let g = cl[4];
+    let b = cl[5];
+    
+    ctx.fillStyle = `rgb(${r},${g},${b})`
+    ctx.fillRect(parseFloat(coordx), parseFloat(coordy), 1, 1);
+    
     break;
     case 'if':
       let ifstatement = cl[1];
@@ -517,7 +365,13 @@ function runTCL(tcs) {
     }
     let cmd_result = runCmd(cmd_list);
     if(cmd_result.startsWith('error')) {
-      addToResultBox(`${cmd_result} on line ${current_line[exec_level]}`);
+      let error_build = `${cmd_result} in ${exec_name[exec_level]} on line ${current_line[exec_level]}`
+      for(let c = exec_level - 1;c >= 0;c--) {
+        error_build += `\n\tcalled from line ${current_line[c]} in ${exec_name[c]}`;
+      }
+      
+      addToResultBox(error_build);
+      
     } else {
       lastval = cmd_result;
     }
